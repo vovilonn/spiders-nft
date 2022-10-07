49e6c31ca7ab45137861309091d41b3989536245
vovilonn <zalevski.v@defilab.space>
2022-10-07 21:57:09 +0400

    fix: safari navbar

--- js/app.js
+++ js/app.js
@@ -105,6 +105,7 @@ window.addEventListener('DOMContentLoaded', () => {
     stepSlider ();
 
     const swiper = new Swiper('.mobile-mirror__slider', {
+        autoHeight: true,
 
         pagination: {
             el: '.mobile-mirror__pagination',
@@ -132,6 +133,18 @@ window.addEventListener('DOMContentLoaded', () => {
     }
 
     mobileSlide();
+
+    // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+    let vh = window.innerHeight * 0.01;
+// Then we set the value in the --vh custom property to the root of the document
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+// We listen to the resize event
+    window.addEventListener('resize', () => {
+        // We execute the same script as before
+        let vh = window.innerHeight * 0.01;
+        document.documentElement.style.setProperty('--vh', `${vh}px`);
+    });
 
 })
 
