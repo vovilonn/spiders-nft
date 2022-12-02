c5c91b067ea1f3e1021fc8ff9fd3c96ebec3c0db
vovilonn <zalevski.v@defilab.space>
2022-12-02 20:02:45 +0200

    fix: colors

--- js/mint.js
+++ js/mint.js
@@ -1,30 +1,30 @@
-var productCounter = {
-    count: 0,
-    incrementCounter: function () {
-        if (this.count < 3) {
-            return (this.count = this.count + 1);
-        }
-        return this.count;
-    },
-    decrementCounter: function () {
-        if (this.count > 1) {
-            return (this.count = this.count - 1);
-        }
-        return (this.count = 1);
-    },
-    resetCounter: function () {
-        return (this.count = 1);
-    },
-};
+// var productCounter = {
+//     count: 0,
+//     incrementCounter: function () {
+//         if (this.count < 3) {
+//             return (this.count = this.count + 1);
+//         }
+//         return this.count;
+//     },
+//     decrementCounter: function () {
+//         if (this.count > 1) {
+//             return (this.count = this.count - 1);
+//         }
+//         return (this.count = 1);
+//     },
+//     resetCounter: function () {
+//         return (this.count = 1);
+//     },
+// };
 
-var displayCout = document.getElementById("displayCounter");
-displayCout.innerHTML = 1;
-document.getElementById("increment").onclick = function () {
-    displayCout.innerHTML = productCounter.incrementCounter();
-};
-document.getElementById("decrement").onclick = function () {
-    displayCout.innerHTML = productCounter.decrementCounter();
-};
+// var displayCout = document.getElementById("displayCounter");
+// displayCout.innerHTML = 1;
+// document.getElementById("increment").onclick = function () {
+//     displayCout.innerHTML = productCounter.incrementCounter();
+// };
+// document.getElementById("decrement").onclick = function () {
+//     displayCout.innerHTML = productCounter.decrementCounter();
+// };
 
 const state = {
     connected: false,
